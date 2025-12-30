cd8a0c0a0ae8e8a335879823722de08589fdba6d
Strcy042 <activa042@gmail.com>
2025-12-30 16:19:37 +0000

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,17 +8,17 @@ from os import environ
 
 
 
-API_ID = int(environ.get("API_ID", "28094744"))
+API_ID = int(environ.get("API_ID", "31815314"))
 
-API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
+API_HASH = environ.get("API_HASH", "480160a314cf23e90f8b9bd89368959a")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "7058856750:AAGCcRzc6ndl9Ae7cpbBCfHfYY7l0h6a9eI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8510187991:AAHd-YtLtrU8M8APaGQRjKVpCqlcq89Auss")
 
-OWNER = int(environ.get("OWNER", "6126688051"))
+OWNER = int(environ.get("OWNER", "7674277191"))
 
 CREDIT = "₹🌐STRANGER🙋‍♂️💠"
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6126688051').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7674277191').split(',')
 
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 
@@ -31,3 +31,4 @@ if int(OWNER) not in AUTH_USERS:
 #WEBHOOK = True  # Don't change this
 
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
